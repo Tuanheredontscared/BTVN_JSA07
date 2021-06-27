35e0a15ef4c8df90ad87e73436e923749cb3f2a0
Tuan <linhdandoannguyen82@gmail.com>
2021-06-27 12:04:03 +0700

    first commit

--- Btbuoi5.js
+++ Btbuoi5.js
@@ -1,16 +1,16 @@
 var i = 0;
-var i = 0;
-function minus() {
+var a = 0;
+var b = 0;
+function minus0() {
     i--;
     document.getElementById('inc').value = i;
 }
 
-function plus() {
+function plus0() {
     i++;
     document.getElementById('inc').value = i;
 }
-var a = 0;
-var a = 0;
+
 function minus1() {
     a--;
     document.getElementById('inc-1').value = a;
@@ -20,8 +20,7 @@ function plus1() {
     a++;
     document.getElementById('inc-1').value = a;
 }
-var b = 0;
-var b = 0;
+
 function minus2() {
     b--;
     document.getElementById('inc-2').value = b;
@@ -31,35 +30,111 @@ function plus2() {
     b++;
     document.getElementById('inc-2').value = b;
 }
-function onclick() {
-    let quantity = document.getElementById('inc').value
-    let project = "Headphone"
-    let thing = {
-        project : project,
-        quantity : quantity
-    } 
-    localStorage.setItem("data", JSON.stringify(thing))
-    document.getElementById('text3').innerHTML = localStorage.getItem("data")
-}
-function onclick() {
-    let quantity_1 = document.getElementById('inc').value
-    let project_1= "Flower pot"
-    let data = {
-        project : project_1,
-        quantity : quantity_1
-    } 
-    localStorage.setItem("data", JSON.stringify(data))
-    document.getElementById('text4').innerHTML = localStorage.getItem("data")
+
+localStorage.setItem("data", JSON.stringify([]))
+document.getElementById("buyheadphone").addEventListener("click", click)
+function click() {
+    let item = {
+        name: "Headphone",
+        quantity: document.getElementById('inc').value,
+        price: 5.5
+    }
+    let data = JSON.parse(localStorage.getItem("data"))
+    if (item.quantity === "0") {
+        for (let r = 0; r < data.length; r++) {
+            let userItem = data[r]
+            if (userItem.name === item.name) {
+                data.splice(r, 1)
+            }
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    } else {
+        let oldItem = []
+
+        for (let r = 0; r < data.length; r++) {
+            let userItem = data[r]
+            if (userItem.name === item.name) {
+                userItem.quantity = item.quantity
+            }
+            else {
+                oldItem.push(userItem)
+            }
+        }
+        if (oldItem.length === data.length) {
+            data.push(item)
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    }
 }
 
-function onclick() {
-    let quantity_2 = document.getElementById('inc').value
-    let project_2 = "Socks"
-    let  product= {
-        project : project_2,
-        quantity : quantity_2
-    } 
-    localStorage.setItem("data", JSON.stringify(product))
-    document.getElementById('text').innerHTML = localStorage.getItem("data")
+document.getElementById("buyflowerpot").addEventListener("click", click1)
+function click1() {
+    let item = {
+        name: "Flower pot",
+        quantity: document.getElementById('inc-1').value,
+        price: 3.0
+    }
+    let data = JSON.parse(localStorage.getItem("data"))
+    if (item.quantity === "0") {
+        for (let d = 0; d < data.length; d++) {
+            let userItem = data[d]
+            if (userItem.name === item.name) {
+                data.splice(d, 1)
+            }
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    } else {
+        let oldItem = []
+
+        for (let d = 0; d < data.length; d++) {
+            let userItem = data[d]
+            if (userItem.name === item.name) {
+                userItem.quantity = item.quantity
+            }
+            else {
+                oldItem.push(userItem)
+            }
+        }
+        if (oldItem.length === data.length) {
+            data.push(item)
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    }
 }
 
+
+document.getElementById("buysocks").addEventListener("click", click2)
+function click2() {
+    let item = {
+        name: "Socks",
+        quantity: document.getElementById('inc-2').value,
+        price: 4.0
+    }
+    let data = JSON.parse(localStorage.getItem("data"))
+    if (item.quantity === "0") {
+        for (let f = 0; f < data.length; f++) {
+            let userItem = data[f]
+            if (userItem.name === item.name) {
+                data.splice(f, 1)
+            }
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    }
+    else {
+        let oldItem = []
+
+        for (let f = 0; f < data.length; f++) {
+            let userItem = data[f]
+            if (userItem.name === item.name) {
+                userItem.quantity = item.quantity
+            }
+            else {
+                oldItem.push(userItem)
+            }
+        }
+        if (oldItem.length === data.length) {
+            data.push(item)
+        }
+        localStorage.setItem("data", JSON.stringify(data))
+    }
+}
